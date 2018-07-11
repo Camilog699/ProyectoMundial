66739b9e613d95505f161c4dee60885aca8c3636
JuanJoseGonGi <juanjose.eng@gmail.com>
2018-07-10 19:46:46 -0500

    Cambios menores

--- src/Vistas/Tablero.java
+++ src/Vistas/Tablero.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Tablero {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Tablero");
+        JFrame frame = new JFrame("Mundial Rusia 2018");
         frame.setContentPane(new Tablero().inicio);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -12,4 +12,5 @@ public class Tablero {
     }
     
     private JPanel inicio;
+    private JLabel bg;
 }
