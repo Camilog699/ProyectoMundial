338d849e26c5b56b1a07fc971246adc419dc15fb
Camilog699 <amraga10@gmail.com>
2018-07-13 11:12:31 -0500

    Nuevo Form de jugadores agregado

--- src/Vistas/verJugadoresPanel.java
+++ src/Vistas/verJugadoresPanel.java
@@ -1,4 +1,22 @@
 package Vistas;
 
+import Modelos.Fondo;
+
+import java.awt.*;
+
 public class verJugadoresPanel extends javax.swing.JPanel{
+
+
+    Fondo bg;
+
+    public verJugadoresPanel() { this.bg = new Fondo("bg", 0, 0, 0 ,0); }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.bg.setAncho(this.getWidth());
+        this.bg.setAlto(this.getHeight());
+        g.drawImage(this.bg.getSprite().getImage(), this.bg.getX(), this.bg.getY(), this.bg.getAncho(), this.bg.getAlto(), this);
+        repaint();
+    }
 }
