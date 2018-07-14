7a86207c13238b245a0b21d1c94902a88f0cc484
Diego Marulanda <quindialejo@gmail.com>
2018-07-14 01:15:56 -0500

    Prueba botones 6

--- src/Vistas/Tablero.java
+++ src/Vistas/Tablero.java
@@ -3,6 +3,7 @@ package Vistas;
 import Clases.Juego;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.LinkedList;
 
@@ -28,9 +29,9 @@ public class Tablero {
             verJugadoresFrame.setLocationRelativeTo(null);
             verJugadoresFrame.setVisible(true);
             verJugadoresFrame.setResizable(false);
-            subirDatosPartidoButton.setBorder(BorderFactory.createLineBorder(Color.RED));
-            verEquiposButton.setBorder(BorderFactory.createLineBorder(Color.RED));
-            verEquiposButton.setBorder(BorderFactory.createLineBorder(Color.RED));
+            subirDatosPartidoButton.setBorder(new LineBorder(Color.RED));
+            verEquiposButton.setBorder(new LineBorder(Color.RED));
+            verEquiposButton.setBorder(new LineBorder(Color.RED));
         });
         subirDatosPartidoButton.addActionListener(l -> {
             subirDatosFrame = new FrameSubirDatos("Subir datos partido | Mundial Russia 2018", juegos);
