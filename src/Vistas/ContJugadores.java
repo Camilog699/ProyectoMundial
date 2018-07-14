f5d8f3d1531c3539d44e7719afe95a1a4a05f30f
JuanJoseGonGi <juanjose.eng@gmail.com>
2018-07-13 20:02:42 -0500

    JSON cambio nombre

---
+++ src/Vistas/ContJugadores.java
@@ -0,0 +1,11 @@
+package Vistas;
+
+import javax.swing.*;
+
+public class ContJugadores {
+    private JPanel panel;
+    
+    private void createUIComponents() {
+        panel = new ContJugadoresPanel();
+    }
+}
