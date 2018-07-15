51d4fe08c5e3651cf9e1a9510b3787b0be88b47d
Diego Marulanda <quindialejo@gmail.com>
2018-07-15 08:07:40 -0500

    a

--- src/Clases/Gol.java
+++ src/Clases/Gol.java
@@ -1,13 +1,40 @@
 package Clases;
 
-public class Gol extends DatoPartido {
+public class Gol {
 
-    public Gol() {
-    }
+    private  String tiempo;
+    private  Equipo equipo;
+    private  Jugador j;
 
     public Gol(String tiempo, Equipo equipo, Jugador j) {
-        super(tiempo, equipo, j);
+
+        this.tiempo = tiempo;
+        this.equipo = equipo;
+        this.j = j;
     }
 
+    public void setTiempo(String tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    public void setJ(Jugador j) {
+        this.j = j;
+    }
+
+    public String getTiempo() {
+        return tiempo;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public Jugador getJ() {
+        return j;
+    }
 
 }
--- src/Vistas/SubirDatos/SubirDatos.java
+++ src/Vistas/SubirDatos/SubirDatos.java
@@ -1,22 +1,30 @@
 package Vistas.SubirDatos;
 
 import Clases.Equipo;
+import Clases.Gol;
+import Clases.Jugador;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class SubirDatos {
     public JPanel panel;
     private JComboBox Equipo;
-    private JComboBox TiroEsquina;
+    private JComboBox Jugador;
     private JLabel gol;
     private JButton backSD;
+    private JTextField minuto;
+    private JButton guardarGol;
     Equipo e1;
     Equipo e2;
     FrameSubirDatos frame;
+    LinkedList<Gol> golesE1 = new LinkedList<>();
+    LinkedList<Gol> golesE2 = new LinkedList<>();
 
     public SubirDatos() {
         panel.setFocusable(true);
@@ -36,6 +44,46 @@ public class SubirDatos {
                 frame = (FrameSubirDatos) SwingUtilities.getWindowAncestor(panel);
                 frame.setVisible(false);
                 frame.dispose();
+            }
+        });
+        minuto.setHorizontalAlignment(JTextField.CENTER);
+        minuto.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                if (minuto.getText().equals("Minuto en el que marcó")){
+                    minuto.setText("");
+                }
+            }
+        });
+        minuto.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                if (minuto.getText().equals("")){
+                    minuto.setText("Minuto en el que marcó");
+                }
+            }
+        });
+        guardarGol.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (Objects.equals(Equipo.getSelectedItem(), "Seleccione equipo")){
+                    JOptionPane.showMessageDialog(null,"Debe seleccionar el quipo al que pertenece el gol");
+                }
+                else if (Objects.equals(Jugador.getSelectedItem(), "Seleccione jugador")){
+                    JOptionPane.showMessageDialog(null,"Debe seleccionar el jugador que hizo el gol");
+                }
+                else if(minuto.getText().equals("Minuto en el que marcó")){
+                    JOptionPane.showMessageDialog(null,"Digite el minuto en el que se marco el gol");
+                }
+                else{
+                    if(Objects.equals(Equipo.getSelectedItem(), e1.getNombre())){
+                        for (Jugador jugador:e1.getJugadores()) {
+                            if (jugador.getNombre().equals())
+                        }
+                    }
+                }
             }
         });
     }
