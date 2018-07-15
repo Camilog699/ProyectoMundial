046d40b2bc3ac918494285ee97ce42daaef91cd1
Diego Marulanda <quindialejo@gmail.com>
2018-07-14 20:05:42 -0500

    Imagen boton back añadido y crearPartido.form mas gonito

--- src/Vistas/CrearPartido/CrearPartido.java
+++ src/Vistas/CrearPartido/CrearPartido.java
@@ -1,14 +1,61 @@
 package Vistas.CrearPartido;
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class CrearPartido{
     public JPanel panel;
     private JComboBox equipo1;
-    private JLabel vs;
     private JComboBox equipo2;
-    private JTextArea Estadio;
+    private JLabel vs;
+    private JTextField estadio;
+    private JTextField horaPartido;
+    private JButton iniciarPartido;
+
+    public CrearPartido() {
+        panel.setFocusable(true);
+        estadio.setHorizontalAlignment(JTextField.CENTER);
+        estadio.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                if (estadio.getText().equals("Estadio")){
+                    estadio.setText("");
+                }
+            }
+        });
+        estadio.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                if (estadio.getText().equals("")){
+                    estadio.setText("Estadio");
+                }
+            }
+        });
+        horaPartido.setHorizontalAlignment(JTextField.CENTER);
+        horaPartido.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                super.focusGained(e);
+                if (horaPartido.getText().equals("Hora del partido")){
+                    horaPartido.setText("");
+                }
+            }
+        });
+        horaPartido.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                super.focusLost(e);
+                if (horaPartido.getText().equals("")){
+                    horaPartido.setText("Hora del partido");
+                }
+            }
+        });
+    }
 
     private void createUIComponents() {
         panel = new CrearPartidoPanel();
+
     }
 }
