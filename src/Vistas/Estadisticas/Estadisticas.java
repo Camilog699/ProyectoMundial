74986095a03c1247d581a7b7950d6715866b15af
JuanJoseGonGi <juanjose.eng@gmail.com>
2018-07-17 00:42:30 -0500

    Goles y tarjetas cargadas

--- src/Vistas/Estadisticas/Estadisticas.java
+++ src/Vistas/Estadisticas/Estadisticas.java
@@ -2,6 +2,7 @@ package Vistas.Estadisticas;
 
 import Clases.Gol;
 import Clases.Juego;
+import Clases.Tarjeta;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,6 +11,7 @@ import java.awt.event.FocusEvent;
 
 public class Estadisticas {
     JPanel golesPanel;
+    JPanel tarjetasPanel;
     private JTabbedPane tabbedPane1;
     Juego juego;
     public JPanel panel;
@@ -27,18 +29,41 @@ public class Estadisticas {
                 juego = frame.getJuego();
                 golesPanel = new JPanel(new GridLayout(juego.getGolesE1().size() + juego.getGolesE2().size(), 2));
                 golesPanel.setPreferredSize(new Dimension(600, juego.getGolesE1().size() + juego.getGolesE2().size() * 70));
+                golesPanel.setOpaque(false);
                 golesScrollPanel.setViewportView(golesPanel);
                 golesScrollPanel.setOpaque(false);
                 golesScrollPanel.getViewport().setOpaque(false);
-                juego.getGolesE1().forEach(gol -> actualiizarGolesPanel(gol));
-                juego.getGolesE2().forEach(gol -> actualiizarGolesPanel(gol));
+                golesScrollPanel.setFont(new javax.swing.plaf.FontUIResource("Dusha V5", Font.PLAIN, 20));
+                juego.getGolesE1().forEach(gol -> actualizarGolesPanel(gol));
+                juego.getGolesE2().forEach(gol -> actualizarGolesPanel(gol));
+                tarjetasPanel = new JPanel(new GridLayout(juego.getGolesE1().size() + juego.getGolesE2().size(), 2));
+                tarjetasPanel.setPreferredSize(new Dimension(600, juego.getGolesE1().size() + juego.getGolesE2().size() * 70));
+                tarjetasPanel.setOpaque(false);
+                tarjetasScrollPanel.setViewportView(tarjetasPanel);
+                tarjetasScrollPanel.setOpaque(false);
+                tarjetasScrollPanel.getViewport().setOpaque(false);
+                tarjetasScrollPanel.setFont(new javax.swing.plaf.FontUIResource("Dusha V5", Font.PLAIN, 20));
+                juego.getTarjetasE1().forEach(tarjeta -> actualizarTarjetasPanel(tarjeta));
+                juego.getTarjetasE2().forEach(tarjeta -> actualizarTarjetasPanel(tarjeta));
             }
         });
     }
     
-    public void actualiizarGolesPanel(Gol gol) {
+    public void actualizarGolesPanel(Gol gol) {
         JLabel jugador = new JLabel(gol.getJ().getNombre());
         JLabel tiempo = new JLabel(gol.getTiempo());
+        golesPanel.add(jugador);
+        golesPanel.add(tiempo);
+    }
+    
+    public void actualizarTarjetasPanel(Tarjeta tarjeta) {
+        JLabel jugador = new JLabel(tarjeta.getJugador().getNombre());
+        JLabel tiempo = new JLabel();
+        if (tarjeta.isAmarilla()) {
+            tiempo.setText("Amarilla");
+        } else {
+            tiempo.setText("Roja");
+        }
         golesPanel.add(jugador);
         golesPanel.add(tiempo);
     }
